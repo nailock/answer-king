7683fdc5d0dd33a2b6b99c837a0cf610a8c857e1
nailock <psyche95@msn.com>
2019-04-28 10:45:48 +0100

    Invalid data unit tests

--- src/test/java/ItemControllerTest.java
+++ src/test/java/ItemControllerTest.java
@@ -9,10 +9,10 @@ import java.math.BigDecimal;
 
 public class ItemControllerTest extends GenericControllerTest {
 
+    private final static String uri = "/item";
+
     @Test
-    public void testItemCreate() throws Exception {
-        String uri = "/item";
-        
+    public void testItemCreate() throws Exception {        
         Item testitem = new Item();
         testitem.setId(1L);
         testitem.setName("Test Item");
@@ -32,6 +32,46 @@ public class ItemControllerTest extends GenericControllerTest {
         Assert.assertEquals(testitem.getId(), returnedItem.getId());
         Assert.assertEquals(testitem.getName(), returnedItem.getName());
         Assert.assertEquals(testitem.getPrice(), returnedItem.getPrice());
+    }
+
+    @Test
+    public void testItemCreate_badName() throws Exception {
+        Item testitem = new Item();
+        testitem.setId(1L);
+        testitem.setName("@@??>><<");
+        testitem.setPrice(new BigDecimal(10));
+
+        String itemJson = testitem.toJson();
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post(uri)
+                              .contentType("application/json")
+                              .content(itemJson)).andReturn();
+
+        int status = result.getResponse().getStatus();
+        String responseContent = result.getResponse().getContentAsString();
+
+        Assert.assertEquals(400, status);
+        Assert.assertEquals(responseContent, "{ \"error\": \"Only letters, numbers and spaces are accepted in the item name.\" }");
+    }
+
+    @Test
+    public void testItemCreate_badPrice() throws Exception {
+        Item testitem = new Item();
+        testitem.setId(1L);
+        testitem.setName("Test Item");
+        testitem.setPrice(new BigDecimal(-1));
+
+        String itemJson = testitem.toJson();
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post(uri)
+                              .contentType("application/json")
+                              .content(itemJson)).andReturn();
+
+        int status = result.getResponse().getStatus();
+        String responseContent = result.getResponse().getContentAsString();
+
+        Assert.assertEquals(400, status);
+        Assert.assertEquals(responseContent, "{ \"error\": \"Price cannot be negative.\" }");
     }
 
 }
